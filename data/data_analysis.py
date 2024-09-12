e743f3619f5f878cd630a11f64fb23851f5a4750
Yanarp Madnug <pranaygundam00@gmail.com>
2024-09-11 21:15:51 -0400

    formatted api reqs

--- data/data_analysis.py
+++ data/data_analysis.py
@@ -5,50 +5,82 @@ import pandas as pd
 # BLS Importing
 import requests
 import json
-import prettytable
 import dotenv as env
 import os
 
 env.load_dotenv()
 BLS_API_KEY = os.getenv('BLS_API_KEY')
 
+def area_item_importing(file_name):
+    file = open(file_name, 'r')
+    codes = list(map(lambda x: tuple(x.split("\t")), file.read().splitlines()))
+    file.close()
+    return pd.DataFrame(codes[1:], columns=codes[0])
+
+
+def bls_data_request(series_ids, start_year, end_year):
+    headers = {'Content-type': 'application/json'}
+    data = json.dumps({"seriesid": series_ids, 
+                       "startyear": start_year, 
+                       "endyear": end_year, 
+                       "registrationKey" : BLS_API_KEY})
+    
+    p = requests.post(f'https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+    json_dict = json.loads(p.text)['Results']
+
+    series_dfs = []
+    for series in json_dict['series']:
+        series_id = series['seriesID']
+        years = []
+        period_names = []
+        values = []
+        for item in series['data']:
+            years.append(item['year'])
+            period_names.append(item['periodName'])
+            values.append(item['value'])
+        
+        series_dfs.append(pd.DataFrame({'Year': years, 'Period': period_names, series_id: values}))
+
+    # Merge the list of DataFrames
+    merged_df = pd.concat(series_dfs, ignore_index=True)
+
+    return merged_df
+
 # Average Prices
-## We build the series ID as AP{seasonal_adjustment}{area_code}{item_code}
+# We build the series ID as AP{seasonal_adjustment}{area_code}{item_code}
 
-# Seasonal Adjustment: S or U
-seasonal_adjustment = "S"
+## Seasonal Adjustment: S (seasonally adjusted) or U (unadjusted)
+avgp_seasonal_adjustment = ["S", "U"]
 
-## Area Codes Importing
-avgp_area_code_file = open('area_codes_avgp.area', 'r')
-avgp_area_codes = list(map(lambda x: tuple(x.split("\t")), avgp_area_code_file.read().splitlines()[1:]))
-avgp_area_code_file.close()
-avgp_area_codes_dict = {code: name for code, name in avgp_area_codes}
+## Area-Item Codes Importing
+avgp_area_df = area_item_importing('area_codes_avgp.area')
+avgp_item_df = area_item_importing('item_codes_avgp.item')
 
-## Item Codes Importing
-avgp_item_code_file = open('item_codes_avgp.item', 'r')
-avgp_item_codes = list(map(lambda x: tuple(x.split("\t")), avgp_item_code_file.read().splitlines()[1:]))
-avgp_item_code_file.close()
-avgp_item_codes_dict = {code: name for code, name in avgp_item_codes}
+AVGP_series = [f"AP{sa}{area}{item}" for sa in avgp_seasonal_adjustment 
+                                     for area in avgp_area_df['area_code'] 
+                                     for item in avgp_item_df['item_code']]
 
-"""
-headers = {'Content-type': 'application/json'}
-data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
-p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/{BLS_API_KEY}', data=data, headers=headers)
-json_data = json.loads(p.text)
-for series in json_data['Results']['series']:
-    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
-    seriesId = series['seriesID']
-    for item in series['data']:
-        year = item['year']
-        period = item['period']
-        value = item['value']
-        footnotes=""
-        for footnote in item['footnotes']:
-            if footnote:
-                footnotes = footnotes + footnote['text'] + ','
-        if 'M01' <= period <= 'M12':
-            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-    output = open(seriesId + '.txt','w')
-    output.write (x.get_string())
-    output.close()
-"""
+# CPI Urban Consumers
+# We build the series ID as CU{seasonal_adjustment}{periodicity}{area_code}{base_code}{item_code}
+
+## Seasonal Adjustment: S (seasonally adjusted) or U (unadjusted)
+cpicu_seasonal_adjustment = ["S", "U"]
+
+## Periodicity: S (semi-annual) or R (monthly)
+cpicu_periodicity = ["R", "S"]
+
+## Base: 1982-1984=100 (S), alternate (A)
+cpicu_base = ["S", "A"]
+
+## Area-Item Codes Importing
+cpicu_area_df = area_item_importing('area_codes_cpicu.area')
+cpicu_item_df = area_item_importing('item_codes_cpicu.item')
+
+print(bls_data_request(['CWSR0000SA111211'], 2010, 2020))
+print(bls_data_request(['CUUR0000SA0L1E', 'CWSR0000SA111211'], 2010, 2020))
+
+CPICU_series = [f"CU{sa}{per}{area}{base}{item}" for sa in cpicu_seasonal_adjustment 
+                                                 for per in cpicu_periodicity 
+                                                 for area in cpicu_area_df['area_code'] 
+                                                 for base in cpicu_base 
+                                                 for item in cpicu_item_df['item_code']]
